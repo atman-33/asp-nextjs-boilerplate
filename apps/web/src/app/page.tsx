ae87943fec682d2ae9d78dea992a2573d62672ad
atman <gpbjk0304@gmail.com>
2024-07-14 15:10:35 +0900

    feat(web): APIで取得したデータ表示を追加

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,30 @@
+'use client'
+
 import Image from "next/image";
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect( () => {
+    fetch('http://localhost:5295/todos')
+    .then(async res => {
+      const data = await res.json();
+      console.log(data);
+      setTodos(data);
+    })
+  },[])
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      {/* fetchしたデータを表示 */}      
+      <div className='my-8'>
+        {todos.map((todo: any) => (
+          <p key={todo.id}>{todo.name}</p>
+        ))}      
+      </div>
+
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
